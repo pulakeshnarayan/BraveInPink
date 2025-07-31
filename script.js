9d92657039b7c9ebf91d40530213eea004a29d74
pulakeshnarayan <pully.narayan@gmail.com>
2025-07-31 13:51:48 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,10 +3,4 @@ const tracker = document.getElementById('tracker-fill');
 const currentAmount = 3000;
 const goal = 10000;
 const percentage = (currentAmount / goal) * 100;
-
-// Animate fill after slight delay
-window.addEventListener('DOMContentLoaded', () => {
-  setTimeout(() => {
-    tracker.style.width = percentage + '%';
-  }, 300);
-});
+tracker.style.width = percentage + '%';
